0f8ab07d5931e8dca7230c4ce4d228a7aff17024
liubinbo <1827775255@qq.com>
2021-10-26 18:29:01 +0800

    feat(auth): feat(auth): feat(deps): 添加了单元测试

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -10,7 +10,7 @@
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'Test',
+  name: 'Vuex',
   setup() {
     const count = ref<number>(0)
     const increment = () => {
