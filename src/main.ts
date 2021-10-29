6687564e919018ec163ad0b43fb0f3c422f1ccf5
liubinbo <1827775255@qq.com>
2021-10-29 11:57:15 +0800

    feat(components): liubinbo

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import styleImport from '@/utils/style-import'
-
+// 你好
 const app = createApp(App)
 styleImport(app).use(router).use(store).mount('#app')
