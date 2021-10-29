a2f1f4a43c3874de505fc2966905b1a175e927ef
Sole-BadGuy <Sole-BadGuy@users.noreply.github.com>
2021-10-29 05:46:29 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter b92588a831f2e0d3aa8eeec3ac990e2e9ecadac5

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-// import ElementPlus from 'element-plus'
-import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import styleImport from '@/utils/style-import'
-
-const app = createApp(App)
-styleImport(app).use(router).use(store).mount('#app')
